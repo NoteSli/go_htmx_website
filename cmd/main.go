76c0f6b575d35c0e3282cb369f7f5ca9e40b7852
Note <elscritchl@outlook.com>
2024-06-19 00:56:36 -0400

    finally got the fonts working... there was some weirdness with sharing the font information, and now we have two copies in the /fonts/ and fonts/..../fonts/ folders

--- cmd/main.go
+++ cmd/main.go
@@ -17,11 +17,13 @@ func main() {
 	fs := http.FileServer(http.Dir("./css"))
 	image_fs := http.FileServer(http.Dir("./images"))
 	font_fs := http.FileServer(http.Dir("./fonts/font-awesome-4.7.0/css"))
+	other_font_fs := http.FileServer(http.Dir("./fonts"))
 
 	http.HandleFunc("/", h1)
 	http.Handle("/css/", http.StripPrefix("/css", fs))
 	http.Handle("/images/", http.StripPrefix("/images", image_fs))
 	http.Handle("/fonts/font-awesome-4.7.0/css/", http.StripPrefix("/fonts/font-awesome-4.7.0/css", font_fs))
+	http.Handle("/fonts/", http.StripPrefix("/fonts", other_font_fs))
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
