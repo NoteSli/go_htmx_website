b3497c45d5a3088e3503d5f287ee2ac5764911d0
Note <elscritchl@outlook.com>
2024-06-19 00:46:21 -0400

    added a LOT of stuff, including trying to set up font-awesome.... with limited success so far

--- cmd/main.go
+++ cmd/main.go
@@ -11,13 +11,17 @@ func main() {
 	fmt.Println("hello world")
 
 	h1 := func(w http.ResponseWriter, r *http.Request) {
-		templ := template.Must(template.ParseFiles("index.html"))
+		templ := template.Must(template.ParseFiles("views/index.html"))
 		templ.Execute(w, nil)
 	}
 	fs := http.FileServer(http.Dir("./css"))
+	image_fs := http.FileServer(http.Dir("./images"))
+	font_fs := http.FileServer(http.Dir("./fonts/font-awesome-4.7.0/css"))
 
 	http.HandleFunc("/", h1)
 	http.Handle("/css/", http.StripPrefix("/css", fs))
+	http.Handle("/images/", http.StripPrefix("/images", image_fs))
+	http.Handle("/fonts/font-awesome-4.7.0/css/", http.StripPrefix("/fonts/font-awesome-4.7.0/css", font_fs))
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
